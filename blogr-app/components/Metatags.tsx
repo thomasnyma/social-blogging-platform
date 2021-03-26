507c35d941a52b40944d2d6b35ee16e6e7b129b7
Thomas Maneschijn <thomasmaneschijn@gmail.com>
2021-03-26 15:05:52 +0100

    Metatags

--- blogr-app/components/Metatags.tsx
+++ blogr-app/components/Metatags.tsx
@@ -1,9 +1,9 @@
 import Head from 'next/head';
 
 export default function Metatags({
-	title = 'The Full Next.js + Firebase Course',
-	description = 'A complete Next.js + Firebase course by Fireship.io',
-	image = 'https://fireship.io/courses/react-next-firebase/img/featured.png',
+	title = 'BLOGR - The Social Blogging Platform',
+	description = 'A Social Blogging Platform suited for your needs.',
+	image = 'https://images.pexels.com/photos/267569/pexels-photo-267569.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
 }) {
 	return (
 		<Head>
@@ -13,6 +13,11 @@ export default function Metatags({
 			<meta name='twitter:description' content={description} />
 			<meta name='twitter:image' content={image} />
 
+			<meta
+				property='og:url'
+				content='https://social-blogging-platform.vercel.app/'
+			/>
+			<meta property='og:type' content='app' />
 			<meta property='og:title' content={title} />
 			<meta property='og:description' content={description} />
 			<meta property='og:image' content={image} />
--- blogr-app/pages/_app.tsx
+++ blogr-app/pages/_app.tsx
@@ -4,12 +4,15 @@ import { Toaster } from 'react-hot-toast';
 import { UserContext } from '../library/context';
 
 import { useUserData } from '../library/hooks';
+import React from 'react';
+import Metatags from '../components/Metatags';
 
 function MyApp({ Component, pageProps }) {
 	const userData = useUserData();
 
 	return (
 		<UserContext.Provider value={userData}>
+			<Metatags />
 			<Navbar />
 			<Component {...pageProps} />
 			<Toaster />
