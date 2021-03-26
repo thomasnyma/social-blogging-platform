3657130727c91e08486962032b61879c30d37199
Thomas Maneschijn <thomasmaneschijn@gmail.com>
2021-03-26 14:52:23 +0100

    Update enter.tsx

--- blogr-app/pages/enter.tsx
+++ blogr-app/pages/enter.tsx
@@ -27,7 +27,7 @@ export default function EnterPage({}) {
 // Sign in with Google buttton
 function SignInButton() {
 	const signInWithGoogle = async () => {
-		await auth.signInWithRedirect(googleAuthProvider);
+		await auth.signInWithPopup(googleAuthProvider);
 	};
 
 	return (
